b15ab63b211eb7576fc4ae14c4371230dd5c26ba
Chen Zhang <chenzhang8722@gmail.com>
2024-03-13 13:39:17 -0400

    use the proper component from streamlit

--- ollama_rag_streamlit.py
+++ ollama_rag_streamlit.py
@@ -30,36 +30,49 @@ class LLM:
         pass
 
 
-def main():
-    # Layout
-    st.sidebar.title("Control Panel")
-    model_selection = st.sidebar.selectbox(
-        "Select the Model",
-        options=["mistral", "llama2", "codellama"],
-    )
-    temperature = st.sidebar.slider(
-        "Temperature",
-        min_value=0.0,
-        max_value=1.0,
-        value=0.5,
-    )
-    uploaded_file = st.sidebar.file_uploader(
-        "Upload a file for RAG",
-        type=['pdf'],
-    )
+# Layout
+st.sidebar.title("Control Panel")
+model_selection = st.sidebar.selectbox(
+    "Select the Model",
+    options=["mistral", "llama2", "codellama"],
+)
+temperature = st.sidebar.slider(
+    "Temperature",
+    min_value=0.0,
+    max_value=1.0,
+    value=0.5,
+)
+uploaded_file = st.sidebar.file_uploader(
+    "Upload a file for RAG",
+    type=['pdf'],
+)
 
-    model = LLM(
-        model_name=model_selection,
-        temperature=temperature,
-    )
+model = LLM(
+    model_name=model_selection,
+    temperature=temperature,
+)
 
-    # Chatbot
-    st.title("Simple OLLAMA+RAG Chatbot")
-    chat_container = st.empty()
-    input_text = st.text_input("Your message:", key="chat_input")
-    if st.button("Send"):
-        chat_container.write_stream(model.llm.stream(input_text))
+# Chatbot
+st.title("Simple OLLAMA+RAG Chatbot")
 
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-if __name__ == "__main__":
-    main()
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+user_prompt = st.chat_input("Ask me anything", key="chat_input")
+
+if user_prompt:
+    # display input prompt
+    with st.chat_message("user"):
+        st.write(user_prompt)
+    
+    # display response
+    with st.spinner("Thinking..."):
+        msg = st.write_stream(model.llm.stream(user_prompt))
+        st.session_state.messages.append({"role": "user", "content": user_prompt})
+        st.session_state.messages.append({"role": "bot", "content": msg})
